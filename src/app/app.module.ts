8cc5a18f1ffe39b173a6657788499a29cb27f70f
Being-Insane-HD <hridehd007@gmail.com>
2022-03-01 18:52:17 +0530

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { TeamComponent } from './team/team.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactComponent,
+    TeamComponent,
+    AppointmentComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
